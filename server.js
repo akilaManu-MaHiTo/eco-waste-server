a4bd91704362e34160e4d4d3285e47feeb89456b
akilaManu <akilamanujith@gmail.com>
2025-09-26 23:58:06 +0530

    feat: enable URL-encoded data parsing in the server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
     origin: `${process.env.SERVER_ORIGIN}`,
