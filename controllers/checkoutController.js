6d68d30601cc76f3a90141c7c7190e2b3e98b95a
akilaManu <akilamanujith@gmail.com>
2025-09-27 00:37:35 +0530

    feat: enhance payment notification handling and user upgrade logic

--- controllers/checkoutController.js
+++ controllers/checkoutController.js
@@ -1,15 +1,28 @@
 const Payment = require("../models/Payhere");
+const User = require("../models/User");
+const Role = require("../models/Role");
 
 exports.notifyPayment = async (req, res) => {
   try {
     const paymentData = req.body;
     console.log("Received payment notification:", paymentData);
-    // Save or update payment
+
+    if (!paymentData.payment_id) {
+      return res.status(400).send("payment_id is missing");
+    }
+
     const payment = await Payment.findOneAndUpdate(
       { payment_id: paymentData.payment_id },
       paymentData,
       { upsert: true, new: true, setDefaultsOnInsert: true }
     );
+    const userType = await Role.findOne({ userType: "Purchased-User" });
+    if (paymentData.custom_1) {
+      await User.findByIdAndUpdate(paymentData.custom_1, {
+        userType: userType._id,
+      });
+      console.log(`User ${paymentData.custom_1} upgraded to premium`);
+    }
 
     console.log("Payment processed:", payment.payment_id);
 
