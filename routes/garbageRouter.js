386d4d529edbc53c95c90187408e61b83c314e13
Buddhila <buddhilalakshan12@gmail.com>
2025-10-15 12:48:57 +0530

    feat: add endpoint to retrieve today's garbage records

--- routes/garbageRouter.js
+++ routes/garbageRouter.js
@@ -4,12 +4,14 @@ const { protect } = require("../controllers/authController");
 const {
   createGarbage,
   getGarbage,
+  getTodayGarbage,
   updateGarbage,
   deleteGarbage,
 } = require("../controllers/garbageController");
 
 router.post("/", protect, createGarbage);
 router.get("/", protect, getGarbage);
+router.get("/today", protect, getTodayGarbage);
 router.put("/:id", protect, updateGarbage);
 router.delete("/:id", protect, deleteGarbage);
 module.exports = router;
